458721d85e0087e7915bf39478f3ab0ba1093d33
JackSparring420 <emanuele.arconte94@gmail.com>
2022-01-18 14:04:23 +0100

    Exercise completed

---
+++ index.php
@@ -0,0 +1,103 @@
+    <?php
+
+        class Persona {
+
+            private $nome;
+            private $cognome;
+            private $dataNascita;
+
+            public function __construct($nome, $cognome) {
+
+                $this -> setNome($nome);
+                $this -> setCognome($cognome);
+            }
+    
+            public function getNome() {
+
+                return $this -> nome;
+            }
+            public function setNome($nome) {
+                $this -> nome = $nome;
+            }
+
+            public function getCognome() {
+
+                return $this -> cognome;
+            }
+            public function setCognome($cognome) {
+                $this -> cognome = $cognome;
+            }
+
+            public function getDataNascita() {
+
+                return $this -> dataNascita;
+            }
+            public function setDataNascita($dataNascita) {
+                $this -> dataNascita = $dataNascita;
+            }
+
+            public function printFullPerson() {
+    
+                return $this -> getNome() . " " . $this -> getCognome() . ": " . $this -> getDataNascita();
+            }
+
+            public function __toString() {
+    
+                return $this -> getNome() . " " . $this -> getCognome(). ": " . $this -> getDataNascita();;
+            }    
+        }
+          
+        class Employee extends Persona {
+            private $stipendio;
+            private $dataAssunzione;
+            
+            public function __construct($stipendio, $dataAssunzione) {
+                
+                $this -> setStipendio($stipendio);
+                $this -> setDataAssunzione($dataAssunzione);
+            }
+            
+            public function getStipendio() {
+                
+                return $this -> stipendio;
+            }
+            public function setStipendio($stipendio) {
+                $this -> stipendio = $stipendio;
+            }
+            
+            public function getDataAssunzione() {
+                
+                return $this -> dataAssunzione;
+            }
+            public function setDataAssunzione($dataAssunzione) {
+                $this -> dataAssunzione = $dataAssunzione;
+            }
+
+            public function printFullEmployee() {
+    
+                return $this -> getNome() . " " . $this -> getCognome() . ": " . $this -> getStipendio() . " (" . $this -> getDataAssunzione() . ")";
+            }
+
+            public function __toString() {
+    
+                return $this -> getNome() . " " . $this -> getCognome() . ": " . $this -> getStipendio() . " (" . $this -> getDataAssunzione() . ")";
+            }
+            
+        }
+        
+        $p1 = new Persona("Emanuele", "Arconte");
+        $p1 -> setDataNascita("20/04/1994");
+        echo $p1 . "<br>";
+        echo $p1 -> printFullPerson(). "<br>";
+
+        $e1 = new Employee("1500", "08/09/2021");
+        $e1 -> setNome("Emanuele");
+        $e1 -> setCognome("Arconte");
+        echo $e1 -> printFullEmployee() . "<br>";
+        echo $e1 . "<br>";
+        ?>
+
+
+
+
+
